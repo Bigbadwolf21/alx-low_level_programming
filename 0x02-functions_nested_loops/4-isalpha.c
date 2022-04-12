9c8ca5e78ec04dfd8e591b2fdf6d315b6e9ab113
Ayodele Ayomide <ayodeledprincipal@gmail.com>
2022-04-12 23:20:17 +0100

    Alpha Male

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,16 +2,18 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * @c: c is an ascii character with a value of 99
+ * @c: c is an ascii character
+ *
  * Return: 1 (if letter)
  */
+
 int _isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
-	else if (c >= 'A' && c <= 'z')
+	else if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
@@ -19,4 +21,4 @@ int _isalpha(int c)
 	{
 		return (0);
 	}
-}	
+}
