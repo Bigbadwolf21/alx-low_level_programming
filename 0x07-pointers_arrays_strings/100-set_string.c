922623123fed96518722897631ef0bc82bdeb891
Ayodele Ayomide <ayodeledprincipal@gmail.com>
2022-04-27 20:44:46 +0100

    value of a pointer to a character

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,12 @@
 include "main.h"
 
+#include "main.h"
+
 /**
  * set_string - sets the value of a pointer to a char
  * @s: content is to be copied
- * @to: string
- * Return: void
+ *@to: string
+ *Return: void
  */
 
 void set_string(char **s, char *to)
