c9c421c0a82e1eefff7af380bbba823b9415309b
Ayodele Ayomide <ayodeledprincipal@gmail.com>
2022-04-29 01:09:34 +0100

    Welcome to recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,14 +8,12 @@
 
 void _puts_recursion(char *s)
 {
-    if (*s != '\0')
-    {
-        putchar(*s);
-        _put_recursion(s + 1);
-    }
-    else
-    {
-        _putchar('\n')
-    }
+if (*s != '\0')
+{
+_putchar(*s);
+_puts_recursion(s + 1);
 }
-
+else
+{
+_putchar('\n');
+}
