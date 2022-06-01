4c9267b2fe0f0814e0c6ea5e563ca3528471031c
Ayodele Ayomide <ayodeledprincipal@gmail.com>
2022-06-01 21:17:07 +0100

    Tread lightly

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,14 +5,13 @@
  * read_textfile - Reads a text file and prints it to POSIX stdout.
  * @filename: A pointer to the name of the file.
  * @letters: The number of letters the
- *           function should read and print.
- *
+ * function should read and print.
  * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ * O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t open, read, write;
+	ssize_t op, rd, wr;
 	char *buffer;
 
 	if (filename == NULL)
@@ -22,18 +21,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 		return (0);
 
-	open = open(filename, O_RDONLY);
-	read = read(open, buffer, letters);
-	write = write(STDOUT_FILENO, buffer, read);
+	op = open(filename, O_RDONLY);
+	rd = read(op, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, rd);
 
-	if (open == -1 || read == -1 || write == -1 || write != read)
+	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
 	{
 		free(buffer);
 		return (0);
 	}
 
 	free(buffer);
-	close(open);
+	close(op);
 
-	return (write);
+	return (wr);
 }
